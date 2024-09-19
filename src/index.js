e35474c50d9f1b26e0a74b7bdd941c233cedc819
Deborah Imaji <deborahoimaji@gmail.com>
2024-09-19 06:58:47 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -4,11 +4,45 @@ function displayWeather(response) {
     let headTemp = response.data.temperature.current;
 
     let cityHeading = document.querySelector("#city-heading");
+    let cityCondElement = document.querySelector("#heading-description");
+    let currentHumidityElement = document.querySelector("#current-humidity");
+    let windSpeedElement = document.querySelector("#current-windspeed");
+    let dayTimeElement = document.querySelector("#current-daytime");
+    let currentDate = new Date(response.data.time * 1000);
+
     cityHeading.innerHTML = response.data.city;
-
+    cityCondElement.innerHTML = response.data.condition.description;
     headTempElement.innerHTML = Math.round(headTemp);
+    currentHumidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 
-    console.log(response.data);
+    dayTimeElement.innerHTML = formatDate(currentDate);
+}
+
+function formatDate(currentDate) {
+
+    let hour = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[currentDate.getDay()];
+
+    if (hour < 10) {
+        hour = `0${hour}`;
+    }
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
