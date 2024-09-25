8c25044c53283afbd74bd702be8bf04a7a8eca25
Deborah Imaji <deborahoimaji@gmail.com>
2024-09-25 20:22:58 +0100

    Injected more weather data

--- src/script.js
+++ src/script.js
@@ -5,8 +5,48 @@ function refreshWeather(response) {
 
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current
-
     temperatureElement.innerHTML = Math.round(temperature);
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+    let windSpeedElement = document.querySelector("#wind-speed");
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+    let date = new Date(response.data.time * 1000);
+
+    let timeElement = document.querySelector("#time");
+    timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
